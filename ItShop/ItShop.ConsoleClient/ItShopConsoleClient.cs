3870b1693887c4cc94d36b4b89c097bae76b9a9d
Dobri <dobromir.v.zlatkov@gmail.com>
2014-09-02 13:15:03 +0300

    Unzip

--- ItShop/ItShop.ConsoleClient/ItShopConsoleClient.cs
+++ ItShop/ItShop.ConsoleClient/ItShopConsoleClient.cs
@@ -1,15 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Data.Entity.Validation;
 using System.Data.Entity;
-using MongoDB.Driver;
-using MongoDB.Bson;
 
 using ItShop.Data;
 using ItShop.Model;
 using ItShop.Data.Migrations;
-using ExcelManager;
+using ZipFileManager;
 
 namespace ItShop.ConsoleClient
 {
@@ -20,24 +16,24 @@ namespace ItShop.ConsoleClient
             Database.SetInitializer(
                 new MigrateDatabaseToLatestVersion<ItShopDbContext, Configuration>());
             var db = new ItShopDbContext();
-            /* 
-             * TEST MILAN
-             */
+            ///* 
+            // * TEST MILAN
+            // */
 
-            ExcelManager.ExcelReader.ReadFromExcel2003File();
-            ExcelWriter.CreateExcel2007PlusFile();
-            /*
-             * END OF TESTS
-             */ 
-            DateTime fromDate = new DateTime(2014, 8, 30, 0, 0, 0);
-            DateTime toDate = new DateTime(2014, 8, 31, 23, 59, 59);
+            //ExcelManager.ExcelReader.ReadFromExcel2003File();
+            //ExcelWriter.CreateExcel2007PlusFile();
+            ///*
+            // * END OF TESTS
+            // */ 
+            //DateTime fromDate = new DateTime(2014, 8, 30, 0, 0, 0);
+            //DateTime toDate = new DateTime(2014, 8, 31, 23, 59, 59);
 
-            var salesInRange = db.Sales
-                .Where(x => x.SaleDate >= fromDate && x.SaleDate <= toDate)
-                .ToList();
+            //var salesInRange = db.Sales
+            //    .Where(x => x.SaleDate >= fromDate && x.SaleDate <= toDate)
+            //    .ToList();
 
-            XMLParser xmlParser = new XMLParser();
-            xmlParser.SaveSalesReportToXML(salesInRange, "report.xml");
+            //XMLParser xmlParser = new XMLParser();
+            //xmlParser.SaveSalesReportToXML(salesInRange, "report.xml");
 
             // Load XML File to save in MSSQL DB
           //  IList<Store> expensesList =  xmlParser.LoadXml("expenses.xml");
@@ -70,67 +66,85 @@ namespace ItShop.ConsoleClient
             //        }
             //    }
             //}
-        }
+            //TEST DOBRI
+            //Extracts zip file
+            ZipExcelParser parserFromExcel = new ZipExcelParser(db);
+            IList<Sale> sales = parserFromExcel.LoadData();// to load data to the server tomorrow
 
-        private void FillStoresExpensesInMongoDB(IList<StoresExpenses> expensesList)
-        {
-            MongoClient client = new MongoClient(); // connect to localhost
-            MongoServer server = client.GetServer();
-            MongoDatabase db = server.GetDatabase("StoreExpensesReports");
-
-            MongoCollection<BsonDocument> expensesReports = db.GetCollection<BsonDocument>("ExpensesReports");
-
-            foreach (var expense in expensesList)
+            foreach (var sale in sales)
             {
-                BsonDocument document = new BsonDocument {
-                    { "StoreId", expense.StoreId },
-                    { "ForDate", expense.ForDate },
-                    { "Amount", expense.Amount.ToString() }
-                };
-
-                expensesReports.Insert(document);
+                Console.Write("Store id:" + sale.StoreId);
+                Console.Write("Sale date:" + sale.SaleDate);
+                Console.WriteLine();
+                foreach (var saleDetail in sale.SaleDetails)
+                {
+                    Console.WriteLine(saleDetail.SalePrice);
+                    Console.WriteLine(saleDetail.Quantity);
+                }
             }
+            //TEST DOBRI
+
+        }
+
+        //private void FillStoresExpensesInMongoDB(IList<StoresExpenses> expensesList)
+        //{
+        //    MongoClient client = new MongoClient(); // connect to localhost
+        //    MongoServer server = client.GetServer();
+        //    MongoDatabase db = server.GetDatabase("StoreExpensesReports");
+
+        //    MongoCollection<BsonDocument> expensesReports = db.GetCollection<BsonDocument>("ExpensesReports");
+
+        //    foreach (var expense in expensesList)
+        //    {
+        //        BsonDocument document = new BsonDocument {
+        //            { "StoreId", expense.StoreId },
+        //            { "ForDate", expense.ForDate },
+        //            { "Amount", expense.Amount.ToString() }
+        //        };
+
+        //        expensesReports.Insert(document);
+        //    }
             
-        }
+        //}
 
-        private static void FillData()
-        {
-            var db = new ItShopDbContext();
-            var store = new Store { StoreName = "Main store" };
-            db.Stores.Add(store);
-            db.SaveChanges();
+        //private static void FillData()
+        //{
+        //    var db = new ItShopDbContext();
+        //    var store = new Store { StoreName = "Main store" };
+        //    db.Stores.Add(store);
+        //    db.SaveChanges();
 
-            var product1 = new Product { ProductName = "Laptop HP 556 Cromebook", BasePrice = 1024.20m };
-            var product2 = new Product { ProductName = "Laptop HP 6T Elitebook", BasePrice = 489.00m };
-            var product3 = new Product { ProductName = "Laptop Toshiba Satellite C50-B-14", BasePrice = 620.00m };
+        //    var product1 = new Product { ProductName = "Laptop HP 556 Cromebook", BasePrice = 1024.20m };
+        //    var product2 = new Product { ProductName = "Laptop HP 6T Elitebook", BasePrice = 489.00m };
+        //    var product3 = new Product { ProductName = "Laptop Toshiba Satellite C50-B-14", BasePrice = 620.00m };
 
-            db.Products.Add(product1);
-            db.Products.Add(product2);
-            db.Products.Add(product3);
-            db.SaveChanges();
+        //    db.Products.Add(product1);
+        //    db.Products.Add(product2);
+        //    db.Products.Add(product3);
+        //    db.SaveChanges();
 
-            var sale = new Sale
-            {
-                StoreId = store.StoreId,
-                SaleDate = DateTime.Now
-            };
+        //    var sale = new Sale
+        //    {
+        //        StoreId = store.StoreId,
+        //        SaleDate = DateTime.Now
+        //    };
 
-            sale.SaleDetails.Add(new SaleDetail
-            {
-                ProductId = product1.ProductId,
-                Quantity = 1,
-                SalePrice = product1.BasePrice * 1.2m
-            });
+        //    sale.SaleDetails.Add(new SaleDetail
+        //    {
+        //        ProductId = product1.ProductId,
+        //        Quantity = 1,
+        //        SalePrice = product1.BasePrice * 1.2m
+        //    });
 
-            sale.SaleDetails.Add(new SaleDetail
-            {
-                ProductId = product3.ProductId,
-                Quantity = 2,
-                SalePrice = product3.BasePrice
-            });
+        //    sale.SaleDetails.Add(new SaleDetail
+        //    {
+        //        ProductId = product3.ProductId,
+        //        Quantity = 2,
+        //        SalePrice = product3.BasePrice
+        //    });
 
-            db.Sales.Add(sale);
-            db.SaveChanges();
-        }
+        //    db.Sales.Add(sale);
+        //    db.SaveChanges();
+        //}
     }
 }
